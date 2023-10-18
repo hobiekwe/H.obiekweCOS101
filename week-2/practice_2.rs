38e1e9d23467a213a7c5d530647efdad6daa2248
JUST HENRY <147877795+hobiekwe@users.noreply.github.com>
2023-10-18 20:24:08 +0100

    Create practice_2.rs

---
+++ week-2/practice_2.rs
@@ -0,0 +1,12 @@
+fn main(){
+	
+	println!(); // prints just a
+
+	println!("hello ");//prints 
+
+	println!("format {} arguements", "some"); //prints format some arguements
+
+	println!("My name is {}. I am in the class of stream 1" "Dagogo William-Jumbo!"); 
+
+	
+}
