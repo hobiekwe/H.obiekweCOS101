5717f4251c1a40b69b11ace7f21c5c16ad6324f0
JUST HENRY <147877795+hobiekwe@users.noreply.github.com>
2023-10-18 21:30:18 +0100

    Update practice_2.rs

--- week-2/practice_2.rs
+++ week-2/practice_2.rs
@@ -1,24 +1,12 @@
 fn main(){
 	
-<<<<<<< Updated upstream
-	println!(); 
-=======
 	println!();
->>>>>>> Stashed changes
 
 	println!("hello ");
 
 	println!("format {} arguements", "some");
-<<<<<<< Updated upstream
-=======
 
 	println!("My name is {}. I am the class rep of stream 1", "Dagogo Williams-Jumbo"); 
->>>>>>> Stashed changes
 
 
-<<<<<<< Updated upstream
-	
 }
-=======
-}
->>>>>>> Stashed changes
