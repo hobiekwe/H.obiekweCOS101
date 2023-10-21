d883f286807004b3e45584fca3ce7988a98860af
JUST HENRY <147877795+hobiekwe@users.noreply.github.com>
2023-10-21 18:06:14 +0100

    Create project_2

--- week-2/project_2.rs
+++ week-2/project_2.rs
@@ -1,19 +1,39 @@
-fn main(){
-    let p:f64 = 450_000.00;
-    let r:f64 = 1_500_000.00;
-    let n:f64 = 750_000.00;
-    let x:f64 = 2_850_000.00;
-    let y:f64 = 250_000.00;
+fn main() {
+let t:f64 = 450_000.0;
+let m:f64 = 1_500_000.0;
+let h:f64 = 750_000.0;
+let d:f64 = 2_850_000.0;
+let a:f64 = 250_00.0;
 
-    //to find sum 
+//quantity
 
-    let s = p + r + n + x + y;
-    println!("Sum of the sales {}", s);
+let o:f64 = 2.0;
+let x:f64 = 1.0;
+let p:f64 = 3.0;
+let e:f64 = 3.0;
+let c:f64 = 1.0; 
 
-    //average 
+//total sales 
 
-    let a = s / 5.0;
-    println!("Average of the sales {}", a);
+let ts = (t * o) + (m * x) + (h * p) + (d * e) + (a * c);
+println!("Total sum of sales {}", ts);
+
+//total quantity 
+let tq = o + x + p + e + c;
+println! ("Total sum of quantity {}", tq);
+
+//average of sales record
+
+let v = ts / tq;
+println! ("Average of sales record {}", v);
+
+
+
+
+
+
+
+
 
 
 }
