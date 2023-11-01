16cc6f8d6d46c729b5cd531bcca65190d5588a95
JUST HENRY <147877795+hobiekwe@users.noreply.github.com>
2023-11-01 16:59:27 +0100

    Week-4 commit

--- week-4/project/src/main.rs
+++ week-4/project/src/main.rs
@@ -1,24 +1,33 @@
-//Rust program to calculatr]e speed of a car
+//Rust program to calculate speed of a car
 
 use std::io;
 
 fn main() {
-    // Prompt the user for input
+    // Ask the user for input: distance in miles
     println!("Enter the distance traveled in miles: ");
     let mut distance = String::new();
     io::stdin().read_line(&mut distance).expect("Failed to read input");
+    let d:f32= distance.trim().parse().expect("Invalid input. Please enter a number.");
 
-    println!("Enter the time taken in hours: ");
+    println!("Enter the time taken in hours");
     let mut time = String::new();
     io::stdin().read_line(&mut time).expect("Failed to read input");
+    let t:f32 = time.trim().parse().expect("Invalid input. Please enter a number.");
 
-    // Parse input as f64 (floating-point number)
-    let distance: f64 = distance.trim().parse().expect("Invalid input. Please enter a number.");
-    let time: f64 = time.trim().parse().expect("Invalid input. Please enter a number.");
+    
+    
+  
+    // Convert miles to kilometers (1 mile = 1.60934 kilometers)
+    let distance_km = d * 1.60934;
+    let speed_kmph = d/t;
+    let speed_mph = distance_km/t;
 
-    // Calculate speed (distance / time)
-    let speed = distance / time;
+    
 
-    // Display the result
-    println!("The speed of the car is: {} miles per hour", speed);
-}  
+
+
+    // Display the results
+    println!("The speed of the car is: {} miles per hour", speed_mph);
+    println!("The distance traveled in kilometers is: {} kilometers", distance_km);
+    println!("The speed of the car is: {} kilometers per hour", speed_kmph);
+}
